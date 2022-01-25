400fcbc1699e3e7b24f674cfabf16231bbd94d6b
Dowdowm <bellanger.guillaume77@gmail.com>
2022-01-25 19:48:39 +0100

    cards et headers

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./header/Header";
+export { default as Carousel } from "./carousel/Carousel";
+export { default as HeaderMain } from "./headerMain/HeaderMain";
+export { default as Cards } from "./cards/Cards";
